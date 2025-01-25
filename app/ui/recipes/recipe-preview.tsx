82b734a3a607814fa64c1d63f10f4a3c6c07befb
jahn123 <ahnjason@usc.edu>
2025-01-25 01:15:25 -0800

    minor style adjustments to center previews

--- app/ui/recipes/recipe-preview.tsx
+++ app/ui/recipes/recipe-preview.tsx
@@ -2,27 +2,23 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { convertMinToHrMin } from '@/app/lib/utils';
 
-export default function RecipePreview({
-  id, name, calories, cookTimeMin
-}: {
+export default function RecipePreview({ id, name, calories, cookTimeMin }: {
   id: string, name: string, calories: number, cookTimeMin: number
 }) {
   const { cookTimeHr, leftoverCookTimeMin } = convertMinToHrMin(cookTimeMin);
 
   return (
-    <div className="rounded-md">
+    <div className="flex justify-center rounded-md w-full">
       <Link
         href={`/dashboard/recipes/${id}/view`}
       >
-        <div>
           <Image
             src="/ramen-recipe-image.jpg"
-            width={450}
-            height={450}
+            width={400}
+            height={300}
             alt={`image of ${name}`}
             className="rounded-md bg-gray-900"
           />
-        </div>
         <div className="rounded-md flex justify-around bg-gray-900 text-white">
           <div>
             {name}
