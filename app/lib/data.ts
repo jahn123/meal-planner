5eba7b6d2934e84283bbcdf613d6e805ef294a77
jahn123 <ahnjason@usc.edu>
2025-01-28 13:47:01 -0800

    fetch limited recipes for search

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,6 +16,55 @@ export async function fetchRecipes() {
   }
 }
 
+const ITEMS_PER_PAGE = 9;
+export async function fetchPagedRecipesResult(query: string, page: number) {
+  noStore();
+  const rowsToSkip = (page - 1) * ITEMS_PER_PAGE;
+
+  try {
+    const recipeData = await sql<Recipe>`
+      SELECT recipe_id, recipe_name, calories, cook_time_min 
+      FROM recipes
+      WHERE recipe_name ILIKE ${`%${query}%`}
+      OFFSET ${rowsToSkip} ROWS
+      FETCH FIRST ${ITEMS_PER_PAGE} ROWS ONLY
+    `;
+    const recipeTagData = await Promise.all(recipeData.rows.map((recipe) => {
+      return sql`
+        SELECT recipe_tags.tag_id, tags.tag_name, tags.tag_icon
+        FROM tags
+        INNER JOIN recipe_tags ON recipe_tags.tag_id=tags.tag_id
+        WHERE recipe_id = ${recipe.recipe_id}
+      `;
+    }));
+
+    // console.log(recipeData.rows.length)
+    return recipeTagData.map((tags, index) => {
+      return {...recipeData.rows[index], tags: tags.rows.map((tag) => {
+        return { tag_id: tag.tag_id, tag_name: tag.tag_name };
+      })};
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function fetchRecipePageCount(query: string) {
+  noStore();
+  try {
+    const data = await sql`
+      SELECT COUNT(*)
+      FROM recipes
+      WHERE recipe_name ILIKE ${`${`%${query}%`}`}
+    `;
+    console.log(data)
+
+    return data.rows[0].count;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 export async function fetchRecipesPreview() {
   noStore();
   try {
