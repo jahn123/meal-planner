a91db876cdea874b19760bad5d3ba2867b0ec93b
jahn123 <ahnjason@usc.edu>
2025-01-28 23:51:52 -0800

    added search to recipe route

--- app/dashboard/recipes/page.tsx
+++ app/dashboard/recipes/page.tsx
@@ -1,10 +1,21 @@
-import { fetchRecipesPreview } from '@/app/lib/data';
-import RecipesTable from '@/app/ui/recipes/recipes-table';
 import Link from 'next/link';
+import { fetchPagedRecipesResult, fetchRecipesPreview } from '@/app/lib/data';
+import RecipesTable from '@/app/ui/recipes/recipes-table';
+import RecipeSearch from '@/app/ui/recipe-search';
 import { PlusIcon } from '@heroicons/react/24/outline';
 
-export default async function Page() {
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const currentPage = Number(searchParams?.page) || 1;
+  const query = searchParams?.query || '';
+
   const recipes = await fetchRecipesPreview();
+  const recipeSearchResult = await fetchPagedRecipesResult(query, currentPage);
 
   return (
     <main>
@@ -17,7 +28,8 @@ export default async function Page() {
           <PlusIcon className="w-6 bg-gray-900" />
         </Link>
       </h1>
-      <RecipesTable recipes={recipes} />
+      <RecipeSearch placeholder="Search Recipes..." />
+      <RecipesTable recipes={recipeSearchResult ? recipeSearchResult : recipes} />
     </main>
   );
 }
