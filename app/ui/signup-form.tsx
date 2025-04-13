0cb02322039a9d7c75b3a12c1c5965b68ca4691b
jahn123 <ahnjason@usc.edu>
2025-04-13 14:35:54 -0700

    rewrote login & signup components to use shadcn components

--- app/ui/signup-form.tsx
+++ app/ui/signup-form.tsx
@@ -1,73 +1,80 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { createUser } from '../lib/actions';
 import { ExclamationCircleIcon } from '@heroicons/react/24/solid';
 import { useActionState } from 'react';
 
-export default function SignUpForm() {
+export default function SignUpForm({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
   const [errorState, formAction, isPending] = useActionState(
     createUser,
     { errors: {}, message: '' },
   );
 
   return (
-    <form action={formAction} className="space-y-3">
-      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8 text-black">
-        <h1>
-          Sign up to continue.
-        </h1>
-        <div className="w-full">
-          <label
-            className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-            htmlFor="username"
-          >
-            Username
-          </label>
-          <div>
-            <input 
-              className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-              id="username"
-              type="text"
-              name="username"
-              placeholder="Username"
-              required
-            />
-          </div>
-          <label
-            className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-            htmlFor="username"
-          >
-            Password
-          </label>
-          <div>
-            <input 
-              className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-              id="password"
-              type="password"
-              name="password"
-              placeholder="Password"
-              required
-            />
-          </div>
-        </div>
-        <button
-          aria-disabled={isPending}
-        >
-          Sign Up
-        </button>
-        <div
-          className="flex h-8 items-end space-x-1"
-          aria-live="polite"
-          aria-atomic="true"
-        >
-          {(errorState.errors.username || errorState.errors.password) && (
-            <>
-              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-              <p className="text-sm text-red-500">{errorState.errors.username ? errorState.errors.username : errorState.errors.password }</p>
-            </>
-          )}
-        </div>
-      </div>
-    </form>
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">Sign Up</CardTitle>
+          <CardDescription>
+            Sign up to continue
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form action={formAction}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="username">Username</Label>
+                {/* <input id="username" type="text" required /> */}
+                <Input id="username" name="username" type="username" required />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input id="password" name="password" type="password" required />
+              </div>
+              <div
+                className="flex h-4 items-end space-x-1"
+                aria-live="polite"
+                aria-atomic="true"
+              >
+                {(errorState.errors.username || errorState.errors.password) && (
+                  <>
+                    <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+                    <p className="text-sm text-red-500">{errorState.errors.username ? errorState.errors.username : errorState.errors.password }</p>
+                  </>
+                )}
+              </div>
+              <Button type="submit" className="w-full">
+                Sign Up
+              </Button>
+              {/* <Button variant="outline" className="w-full">
+                Login with Google
+              </Button> */}
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Already have an account?{" "}
+              <a href="/login" className="underline underline-offset-4">
+                Login
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
